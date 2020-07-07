afbbb4d17c8e6296fb742cfe024c2044e3c1c696
Dayo Jaiye <dayojaiye74@gmail.com>
2020-07-07 16:17:19 +0100

    Splash Screen in Android - Kotlin

---
+++ app/src/main/java/com/dev_app/splashscreendemo/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.dev_app.splashscreendemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 3000 //... 3 secs
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        },SPLASH_TIME)
+    }
+}
